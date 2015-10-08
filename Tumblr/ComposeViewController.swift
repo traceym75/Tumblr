5cbc5a05053684ad17547921f376ac7f0a23f3c8
Tracey Ewart <traceym75@gmail.com>
2015-10-07 23:41:49 -0700

    initial commit
    all view controllers are setup; tab images in place

---
+++ Tumblr/ComposeViewController.swift
@@ -0,0 +1,35 @@
+//
+//  ComposeViewController.swift
+//  Tumblr
+//
+//  Created by Tracey Ewart on 10/7/15.
+//  Copyright © 2015 Tracey Ewart. All rights reserved.
+//
+
+import UIKit
+
+class ComposeViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
